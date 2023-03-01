73f7803cef8cd18acdfe549d5a8abe93b105151f
YapKahKeat <yapkahkeat@hotmail.com>
2023-03-01 17:35:27 +0800

    added map and marker

--- Screens/CustomerHome.js
+++ Screens/CustomerHome.js
@@ -1,8 +1,7 @@
-// #6 Email Authentication using Firebase Authentication in React Native App
-// https://aboutreact.com/react-native-firebase-authentication/
+
 
 // Import React and Component
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState,Component } from "react";
 import {
   View,
   Text,
@@ -11,142 +10,41 @@ import {
   TouchableOpacity,
   Alert,
 } from "react-native";
-
-import auth from "@react-native-firebase/auth";
+import MapView,{Marker} from 'react-native-maps';
 
 const CustomerHome = ({ navigation }) => {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged((user) => {
-      console.log("user", JSON.stringify(user));
-      setUser(user);
-    });
-
-    return subscriber;
-  }, []);
-
-  const logout = () => {
-    Alert.alert(
-      "Logout",
-      "Are you sure? You want to logout?",
-      [
-        {
-          text: "Cancel",
-          onPress: () => {
-            return null;
-          },
-        },
-        {
-          text: "Confirm",
-          onPress: () => {
-            auth()
-              .signOut()
-              .then(() => navigation.replace("Login"))
-              .catch((error) => {
-                console.log(error);
-                if (error.code === "auth/no-current-user")
-                  navigation.replace("Login");
-                else alert(error);
-              });
-          },
-        },
-      ],
-      { cancelable: false }
-    );
+  const tokyoRegion = {
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
   };
-
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, padding: 16 }}>
-        <View
-          style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 20,
-              textAlign: "center",
-              marginBottom: 16,
-            }}
-          >
-            Firebase Auth
-          </Text>
-          {user ? (
-            <Text>
-              Welcome{" "}
-              {user.displayName
-                ? user.displayName
-                : user.email}
-            </Text>
-          ) : null}
-          <TouchableOpacity
-            style={styles.buttonStyle}
-            activeOpacity={0.5}
-            onPress={logout}
-          >
-            <Text style={styles.buttonTextStyle}>
-              Logout
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.buttonStyle}
-            activeOpacity={0.5}
-            onPress={() =>
-                navigation.navigate("Customer")
-              }
-          >
-            <Text style={styles.buttonTextStyle}>
-              To Customer View
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <Text
-          style={{
-            fontSize: 18,
-            textAlign: "center",
-            color: "grey",
-          }}
-        >
-          React Native Firebase Authentication
-        </Text>
-        <Text
-          style={{
-            fontSize: 16,
-            textAlign: "center",
-            color: "grey",
-          }}
-        >
-          www.aboutreact.com
-        </Text>
-      </View>
-    </SafeAreaView>
+  return(
+    <View style={styles.container}>
+    <MapView
+    style={StyleSheet.absoluteFill}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+          <Marker coordinate={tokyoRegion} />
+      </MapView>
+    </View>
+    
   );
-};
+
+
+}
 
 export default CustomerHome;
-
 const styles = StyleSheet.create({
-  buttonStyle: {
-    minWidth: 300,
-    backgroundColor: "#7DE24E",
-    borderWidth: 0,
-    color: "#FFFFFF",
-    borderColor: "#7DE24E",
-    height: 40,
-    alignItems: "center",
-    borderRadius: 30,
-    marginLeft: 35,
-    marginRight: 35,
-    marginTop: 20,
-    marginBottom: 25,
+  
+  container: {
+    flex:0.8,
+   
   },
-  buttonTextStyle: {
-    color: "#FFFFFF",
-    paddingVertical: 10,
-    fontSize: 16,
-  },
+ 
 });
